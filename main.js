f301e849d0a38b21ebca0f8baad08d8ab7630dae
No-Max <malyavko.max@gmail.com>
2021-08-09 21:13:13 +0300

    add inheritance

--- main.js
+++ main.js
@@ -12,10 +12,16 @@ const items = [
   "Печенье",
 ];
 
-class Search {
+class Component {
+  constructor(selector) {
+    this.$element = document.querySelector(selector);
+  }
+}
+
+class Search extends Component {
   _value = "";
   constructor(selector, onInput) {
-    this.$element = document.querySelector(selector);
+    super(selector);
     this.$element.addEventListener("input", ({ target }) => {
       this._value = target.value;
       onInput(this._value.toLowerCase());
@@ -23,9 +29,9 @@ class Search {
   }
 }
 
-class Results {
+class Results extends Component {
   constructor(selector, items = []) {
-    this.$element = document.querySelector(selector);
+    super(selector);
     this._items = items;
     this._searchResults = items;
     this.render(); // довольно опасно вызывать методы в конструкторе, но если вы уверены в своем коде, то можно
@@ -45,6 +51,27 @@ class Results {
     );
   }
 }
+
+class CreateButton extends Component {
+  _disabled;
+  _onClick = () => {};
+  constructor(selector) {
+    super(selector);
+    this.disabled = true;
+  }
+
+  set onClick(handler) {
+    this._onClick = handler;
+    this.$element.addEventListener("click", this._onClick);
+  }
+
+  set disabled(isDisabled) {
+    this._disabled = isDisabled;
+    this.$element.disabled = isDisabled;
+  }
+}
+
+const createButton = new CreateButton("#create");
 
 const results = new Results("#results", items);
 
