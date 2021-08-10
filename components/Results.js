76633c13bbc62eecf8471de31668268336abc2bc
No-Max <malyavko.max@gmail.com>
2021-08-10 12:51:39 +0300

    split app on modules

---
+++ components/Results.js
@@ -0,0 +1,34 @@
+import Component from "./Component.js";
+
+export default class Results extends Component {
+  constructor(selector, items = []) {
+    super(selector);
+    this._items = items;
+    this._searchResults = items;
+    this.render(); // довольно опасно вызывать методы в конструкторе, но если вы уверены в своем коде, то можно
+  }
+
+  searchItems(text) {
+    this._searchResults = this._items.filter((item) =>
+      item.toLowerCase().includes(text)
+    );
+    this.render();
+  }
+
+  get items() {
+    return this._items;
+  }
+
+  addItem(item) {
+    this._items.push(item);
+    this._searchResults = this._items;
+    this.render();
+  }
+
+  render() {
+    this.$element.innerHTML = this._searchResults.reduce(
+      (result, item) => (result += `<li>${item}</li>`),
+      ""
+    );
+  }
+}
